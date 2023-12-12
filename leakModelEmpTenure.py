e8a20a4b726efdcf71b11670375de05f387b8e85
Mckay Anderson <andmckay01@gmail.com>
2023-12-12 15:21:48 -0700

    removed foreman_tenure form avg_emp_tenure as it was introducing a VIF.

--- leakModelEmpTenure.py
+++ leakModelEmpTenure.py
@@ -1,20 +1,21 @@
 import pandas as pd
 import statsmodels.api as sm
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 # Load your data
 # Make sure this is the correct file path
-data = pd.read_csv('logit_data_12_11.csv')
+data = pd.read_csv('logit_data_12_12.csv')
 
 # Select predictor variables and the target variable
 predictor_vars = [
     'ROOF_PITCH_BUCKET',
     'YEARS_SINCE_INSTALL',
     'LIQUID_PRECIP_BUCKET',
-    'ROOF_CONCRETE_S_TILE',
-    'RACK_RL',
-    'RACK_S',
-    'RACK_UR',
-    'RACK_TOPSPEED',
+    # 'ROOF_CONCRETE_S_TILE',
+    # 'RACK_RL',
+    # 'RACK_S',
+    # 'RACK_UR',
+    # 'RACK_TOPSPEED',
     'FOREMAN_TENURE_YRS',
     'AVG_EMP_TENURE_YRS'
 ]
@@ -39,3 +40,13 @@ log_reg_model = sm.Logit(y, X).fit()
 
 # Print the summary of the model
 print(log_reg_model.summary())
+
+# Calculate VIFs for each predictor variable
+vif_data = pd.DataFrame()
+vif_data['Feature'] = X.columns
+vif_data['VIF'] = [variance_inflation_factor(
+    X.values, i) for i in range(X.shape[1])]
+
+# Print the VIFs
+print("\nVariance Inflation Factor (VIF) for each feature:")
+print(vif_data)
