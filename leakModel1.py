e9a66a74d50cc65acaca1672632c828b65259e52
Mckay Anderson <andmckay01@gmail.com>
2023-12-11 17:32:32 -0700

    added new variables to data and ran new models

---
+++ leakModel1.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.impute import SimpleImputer
+import math
+
+
+def log_odds_to_probability(log_odds):
+    """Convert log odds to probability."""
+    odds = math.exp(log_odds)
+    probability = odds / (1 + odds)
+    return probability
+
+
+data = pd.read_csv('corr_data_12_11.csv')
+
+# Select your predictor variables and the target variable
+# Replace these column names with the names of your variables
+predictor_vars = ['ROOF_PITCH_BUCKET',
+                  'MONTHS_SINCE_INSTALL',  # included to get a better view of foreman tenure
+                  'LIQUID_PRECIP_BUCKET',
+                  'ROOF_CONCRETE_S_TILE',
+                  'RACK_RL',
+                  'RACK_S',
+                  'RACK_UR',
+                  'RACK_TOPSPEED',
+                  'FOREMAN_TENURE_YRS',
+                  'AVG_EMP_TENURE_YRS'
+                  ]
+
+target_var = 'ROOF_LEAK_FLAG'
+
+# Impute missing values
+imputer = SimpleImputer(strategy='mean')
+data[predictor_vars] = imputer.fit_transform(data[predictor_vars])
+
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    data[predictor_vars], data[target_var], test_size=0.3, random_state=42)
+
+# Create and train the model
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
+
+# Predictions
+predictions = model.predict(X_test)
+
+
+# Evaluation
+print("Confusion Matrix:")
+print(confusion_matrix(y_test, predictions))
+print("\nClassification Report:")
+print(classification_report(y_test, predictions))
+print("Intercept (Bias):", model.intercept_)
+print("Coefficients:")
+for feature, coef in zip(predictor_vars, model.coef_[0]):
+    print(f"{feature}: {coef}")
+    print(f"{feature}_prob: {log_odds_to_probability(coef)}")
---
+++ leakModel2.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import statsmodels.api as sm
+
+# Load your data
+# Make sure this is the correct file path
+data = pd.read_csv('logit_data_12_11.csv')
+
+# Select predictor variables and the target variable
+predictor_vars = [
+    'ROOF_PITCH_BUCKET',
+    'YEARS_SINCE_INSTALL',
+    'LIQUID_PRECIP_BUCKET',
+    'ROOF_CONCRETE_S_TILE',
+    'RACK_RL',
+    'RACK_S',
+    'RACK_UR',
+    'RACK_TOPSPEED',
+    'FOREMAN_TENURE_YRS',
+    'AVG_EMP_TENURE_YRS'
+]
+target_var = 'ROOF_LEAK_FLAG'
+
+# Drop rows with missing values
+data.dropna(subset=predictor_vars + [target_var], inplace=True)
+
+# Define the independent variables (add a constant term to represent the intercept)
+X = sm.add_constant(data[predictor_vars])
+
+# Define the dependent variable
+y = data[target_var]
+
+# Fit the logistic regression model
+log_reg_model = sm.Logit(y, X).fit()
+
+# Print the summary of the model
+print(log_reg_model.summary())
